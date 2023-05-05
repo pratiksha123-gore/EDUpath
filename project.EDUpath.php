7f011d3906e773f695d44528774a46dc70f3c8b4
pratiksha123-gore <126436917+pratiksha123-gore@users.noreply.github.com>
2023-05-05 18:02:42 +0530

    Add files via upload

---
+++ project.EDUpath.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+  <style>
+    
+.header {
+  padding: 6px;
+  background: #00004d;
+  color: white;
+  
+  }
+  .header p{
+    margin-left: 50px;
+  }
+
+       
+nav {
+    display: flex;
+    color:black;
+    font-weight: bold;
+    margin-left: 50px;
+
+}
+nav h1 {
+    font-weight: bold;
+    font-weight: 1500;
+}
+nav ul  {
+    display: flex;
+    color:black;
+    margin-left: 300px;
+
+}
+
+nav ul li{
+
+      margin-right: 0px;
+}
+nav ul li a{
+    color:black;
+    padding: 7px 14px;
+}
+
+nav ul li a:hover{
+    color:black;
+}
+
+.cta{
+    background-color: ;
+    color:black ;
+}
+
+.cta:hover{
+    background-color: ;
+    color: black;
+}
+.button {
+  background-color: #D5F3FE;
+  border: none;
+  color: #3C99DC;
+  padding: 15px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+    </style>
+    <style>
+        body{
+
+
+           font-family: 'Quicksand', sans-serif;
+}
+a{
+    text-decoration: none;
+}
+
+li{
+    list-style-type: none;
+}
+
+ 
+ </style>
+           <!-- This is the code for the header at the upsider of website -->
+           <!-- Style reference === .header -->
+           
+        <div class="header">
+        <p>Working time: Monday to Friday 9 AM to 5 AM &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-facebook-f"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-instagram"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-linkedin-in">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-twitter"></i></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-brands fa-youtube"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Call for free Consultation: 0056 693 5520</p>
+        </div>
+
+        <nav>
+
+        <h1>EduPath</h1>
+            
+        <ul>
+           
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">About</a></li>
+            <li><a href="contact.asp">Services</a></li>
+            <li><a href="about.asp">Project</a></li>
+            <li><a href="about.asp">Courses</a></li>
+            <li><a href="about.asp">Blog</a></li>
+            <li><a href="about.asp"></a></li>
+        </ul>
+
+        <a href="#" class="button">Link Button </a>
+
+            
+        </nav>
+        <img src=/images/first.png width=1500px>
+        <img src=/images/second.png width=1500px> 
+
+
+
+</body>
+</html>
